f08efafa2140cffe5fb30db34b1c04f9891f92fd
Aliou Ba <alioubafr@gmail.com>
2018-08-21 11:05:00 +0200

    Allow all egress

--- template.tf
+++ template.tf
@@ -80,6 +80,13 @@ resource "aws_security_group" "sg-ssh-http" {
     cidr_blocks =  ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
   vpc_id="${aws_vpc.vpc1.id}"
 
   tags {
